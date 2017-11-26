2d8ec8c411dfb31cd41092d6f13e9f51ed6bc8f5
metalflow <metal.flow@gmail.com>
2017-11-26 12:21:46 -0600

    Initial commit

---
+++ Source/Ethos/EthosGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EthosGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEthosGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEthosGameMode();
+};
+
+
+
