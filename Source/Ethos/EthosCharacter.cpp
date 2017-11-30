9ae083771c9a5a1a487da8bcc92598a33675332d
metalflow <metal.flow@gmail.com>
2017-11-29 20:30:07 -0600

    trying to update ethschar files

--- Source/Ethos/EthosCharacter.cpp
+++ Source/Ethos/EthosCharacter.cpp
@@ -11,9 +11,19 @@
 
 //////////////////////////////////////////////////////////////////////////
 // AEthosCharacter
+// This is the NPC actor class, the PC actor class will be made from this
+// one and contain addtional information, plus have all the camera and
+// input information uncommented
+//////////////////////////////////////////////////////////////////////////
 
 AEthosCharacter::AEthosCharacter()
 {
+	//cutom settings
+	//all NPCs must be able to tick
+	PrimaryActorTick.bCanEverTick = true;
+	
+
+
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
@@ -22,9 +32,9 @@ AEthosCharacter::AEthosCharacter()
 	BaseLookUpRate = 45.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
+	//bUseControllerRotationPitch = false;
+	//bUseControllerRotationYaw = false;
+	//bUseControllerRotationRoll = false;
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
@@ -33,15 +43,15 @@ AEthosCharacter::AEthosCharacter()
 	GetCharacterMovement()->AirControl = 0.2f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	//CameraBoom->SetupAttachment(RootComponent);
+	//CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	//CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	//FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	//FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	//FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
@@ -50,85 +60,85 @@ AEthosCharacter::AEthosCharacter()
 //////////////////////////////////////////////////////////////////////////
 // Input
 
-void AEthosCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
-{
-	// Set up gameplay key bindings
-	check(PlayerInputComponent);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+//void AEthosCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+//{
+//	// Set up gameplay key bindings
+//	check(PlayerInputComponent);
+//	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+//	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+//
+//	PlayerInputComponent->BindAxis("MoveForward", this, &AEthosCharacter::MoveForward);
+//	PlayerInputComponent->BindAxis("MoveRight", this, &AEthosCharacter::MoveRight);
+//
+//	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+//	// "turn" handles devices that provide an absolute delta, such as a mouse.
+//	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+//	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+//	PlayerInputComponent->BindAxis("TurnRate", this, &AEthosCharacter::TurnAtRate);
+//	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+//	PlayerInputComponent->BindAxis("LookUpRate", this, &AEthosCharacter::LookUpAtRate);
+//
+//	// handle touch devices
+//	PlayerInputComponent->BindTouch(IE_Pressed, this, &AEthosCharacter::TouchStarted);
+//	PlayerInputComponent->BindTouch(IE_Released, this, &AEthosCharacter::TouchStopped);
+//
+//	// VR headset functionality
+//	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AEthosCharacter::OnResetVR);
+//}
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AEthosCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AEthosCharacter::MoveRight);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AEthosCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &AEthosCharacter::LookUpAtRate);
+//void AEthosCharacter::OnResetVR()
+//{
+//	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+//}
 
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &AEthosCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &AEthosCharacter::TouchStopped);
+//void AEthosCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
+//{
+//		Jump();
+//}
 
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AEthosCharacter::OnResetVR);
-}
+//void AEthosCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
+//{
+//		StopJumping();
+//}
 
+//void AEthosCharacter::TurnAtRate(float Rate)
+//{
+//	// calculate delta for this frame from the rate information
+//	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+//}
 
-void AEthosCharacter::OnResetVR()
-{
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
+//void AEthosCharacter::LookUpAtRate(float Rate)
+//{
+//	// calculate delta for this frame from the rate information
+//	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+//}
 
-void AEthosCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		Jump();
-}
+//void AEthosCharacter::MoveForward(float Value)
+//{
+//	if ((Controller != NULL) && (Value != 0.0f))
+//	{
+//		// find out which way is forward
+//		const FRotator Rotation = Controller->GetControlRotation();
+//		const FRotator YawRotation(0, Rotation.Yaw, 0);
+//
+//		// get forward vector
+//		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+//		AddMovementInput(Direction, Value);
+//	}
+//}
 
-void AEthosCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		StopJumping();
-}
-
-void AEthosCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AEthosCharacter::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AEthosCharacter::MoveForward(float Value)
-{
-	if ((Controller != NULL) && (Value != 0.0f))
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
-	}
-}
-
-void AEthosCharacter::MoveRight(float Value)
-{
-	if ( (Controller != NULL) && (Value != 0.0f) )
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
-	}
-}
+//void AEthosCharacter::MoveRight(float Value)
+//{
+//	if ( (Controller != NULL) && (Value != 0.0f) )
+//	{
+//		// find out which way is right
+//		const FRotator Rotation = Controller->GetControlRotation();
+//		const FRotator YawRotation(0, Rotation.Yaw, 0);
+//	
+//		// get right vector 
+//		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+//		// add movement in that direction
+//		AddMovementInput(Direction, Value);
+//	}
+//}
